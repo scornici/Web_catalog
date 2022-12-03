c316219b2b6010c5db127a836b0b287e2905dead
ionscornici <90756892+ionscornici@users.noreply.github.com>
2022-12-03 16:57:24 +0200

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="https://github.com/scornici">Github Scornici</a></li>
+				<li><a href="https://www.instagram.com/mititelu4467/">Instagram Mititelu</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> Docker Presentation</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
